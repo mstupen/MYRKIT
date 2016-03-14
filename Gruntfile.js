21709aabe12ff3171adf231000e3a6b75faa19dd
Roksolana <mariana.stupen@gmail.com>
2016-03-14 10:57:10 +0200

    new files

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,30 +8,65 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'src/js/<%= pkg.name %>.js',
+        dest: 'dist/js/<%= pkg.name %>.min.js'
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['src/*'],
+        tasks: ['copy'],
+        options: {
+          spawn: false,
+        },
       }
     },
     copy: {
-      bootstrap: {
+      main: {
         files: [{
-            expand: true,
-            cwd: 'bower_components/bootstrap/dist/',
-            src: [
-                'css/bootstrap.css',
-                'css/bootstrap.min.css',
-                'js/bootstrap.js',
-                'js/bootstrap.min.js'
-            ],
-            dest: 'dist/'
+          expand: true,
+          cwd: 'src/',
+          src: [
+            '*.html',
+            'js/holder.min.js',
+            'js/ie10-viewport-bug-workaround.js',
+            'css/ie10-viewport-bug-workaround.css'
+          ],
+          dest: 'dist/'
         }, ]
       },
+      bootstrap: {
+        files: [{
+          expand: true,
+          cwd: 'bower_components/bootstrap/dist/',
+          src: [
+            'css/bootstrap.css',
+            'css/bootstrap.min.css',
+            'js/bootstrap.js',
+            'js/bootstrap.min.js',
+            'fonts/*'
+          ],
+          dest: 'dist/'
+        }, ]
+      },
+      jquery: {
+        files: [{
+          expand: true,
+          cwd: 'bower_components/jquery/dist/',
+          src: [
+            'jquery.js',
+            'jquery.min.js'
+          ],
+          dest: 'dist/js'
+        }, ]
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify', 'copy']);
---
+++ dist/js/myrkit.min.js
@@ -0,0 +1,56 @@
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+/* copy loaded thumbnails into carousel */
+$('.row .thumbnail').on('load', function() {
+
+}).each(function(i) {
+  if(this.complete) {
+  	var item = $('<div class="item"></div>');
+    var itemDiv = $(this).parents('div');
+    var title = $(this).parent('a').attr("title");
+
+    item.attr("title",title);
+  	$(itemDiv.html()).appendTo(item);
+  	item.appendTo('.carousel-inner');
+    if (i==0){ // set first item active
+     item.addClass('active');
+    }
+  }
+});
+
+/* activate the carousel */
+$('#photoCarousel').carousel({interval:false});
+
+/* change modal title when slide changes */
+$('#photoCarousel').on('slid.bs.carousel', function () {
+  $('.modal-title').html($(this).find('.active').attr("title"));
+})
+
+/* when clicking a thumbnail */
+$('.row .thumbnail').click(function(){
+    var idx = $(this).parents('div').index();
+  	var id = parseInt(idx);
+  	$('#myModal').modal('show'); // show the modal
+    $('#photoCarousel').carousel(id); // slide carousel to selected
+
+});
